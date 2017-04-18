34b212c9489ce2c66afabefe7a4f2735aeafcbd8
justin <justin@justin-ThinkPad-P50>
2017-04-18 11:33:43 +0200

    Clean up the Crypter class
    remove System.outs
    add javadoc
    comment the INIT_VECTOR

--- IntegrationProject/src/tests/TransportLayerTest.java
+++ IntegrationProject/src/tests/TransportLayerTest.java
@@ -6,85 +6,57 @@ import org.junit.Before;
 import org.junit.Test;
 
 import connection.TransportLayer;
-import encryption.Crypter;
-import encryption.EncryptionPair;
 import packet.*;
 
 public class TransportLayerTest {
 	
 	Pulse pulse;
-	PlainMessage plainMessage;
-	EncryptedMessage encryptedMessage;
+	GlobalMessage globalMessage;
 	Acknowledgement acknowledgement;
 	
 	Packet pulsePacket;
-	Packet encrMsgPacket;
 	Packet ackPacket;
 	
-	// For the encryption test
-	String plainText = "Hello A, this is B";
-	int prime = 23;
-	int generator = 5;
-	int secretKeyA = 18;
-	int secretKeyB = 21;
-	
-	EncryptionPair epA = new EncryptionPair(23, 5, 18, true);
-	EncryptionPair epB = new EncryptionPair(23, 5, 21, true);
-	
-	String midWayKeyA = Crypter.getKey(epA, secretKeyA);
-	String midWayKeyB = Crypter.getKey(epB, secretKeyB);
-	
-	String cipher = Crypter.encrypt(midWayKeyA, plainText);
-	
 	@Before
-	public void setUp() {
+	public void setUp() {		
 		pulse = new Pulse(3, "Bob");
-		plainMessage = new PlainMessage(20, 5, "Hello");
-		encryptedMessage = new EncryptedMessage(33, Integer.parseInt(midWayKeyA), cipher.length(), cipher);
+		globalMessage = new GlobalMessage(20, 5, "Hello");
 		acknowledgement = new Acknowledgement(20);
 		
 		pulsePacket = new Packet(1, 2, 1, 0, pulse);
-		encrMsgPacket = new Packet(2, 1, 2 , 1, encryptedMessage);
 		ackPacket = new Packet(4, 3, 4, 2, acknowledgement);
+		
 	}
 	
 	@Test
 	public void getSenderIDTest() {
 		assertEquals(1, TransportLayer.getSenderID(pulsePacket.getDatagramPacketData()));
-		assertEquals(2, TransportLayer.getSenderID(encrMsgPacket.getDatagramPacketData()));
 		assertEquals(4, TransportLayer.getSenderID(ackPacket.getDatagramPacketData()));
 		assertEquals(1, pulsePacket.getSenderID());
-		assertEquals(2, encrMsgPacket.getSenderID());
 		assertEquals(4, ackPacket.getSenderID());
 	}
 	
 	@Test
 	public void getReceiverIDTest() {
 		assertEquals(2, TransportLayer.getReceiverID(pulsePacket.getDatagramPacketData()));
-		assertEquals(1, TransportLayer.getReceiverID(encrMsgPacket.getDatagramPacketData()));
 		assertEquals(3, TransportLayer.getReceiverID(ackPacket.getDatagramPacketData()));
 		assertEquals(2, pulsePacket.getReceiverID());
-		assertEquals(1, encrMsgPacket.getReceiverID());
 		assertEquals(3, ackPacket.getReceiverID());
 	}
 	
 	@Test
 	public void getSequenceNumberTest() {
 		assertEquals(1, TransportLayer.getSequenceNumber(pulsePacket.getDatagramPacketData()));
-		assertEquals(2, TransportLayer.getSequenceNumber(encrMsgPacket.getDatagramPacketData()));
 		assertEquals(4, TransportLayer.getSequenceNumber(ackPacket.getDatagramPacketData()));
 		assertEquals(1, pulsePacket.getSequenceNumber());
-		assertEquals(2, encrMsgPacket.getSequenceNumber());
 		assertEquals(4, ackPacket.getSequenceNumber());		
 	}
 	
 	@Test
 	public void getTypeIdentifiefTest() {
 		assertEquals(0, TransportLayer.getTypeIdentifier(pulsePacket.getDatagramPacketData()));
-		assertEquals(1, TransportLayer.getTypeIdentifier(encrMsgPacket.getDatagramPacketData()));
 		assertEquals(2, TransportLayer.getTypeIdentifier(ackPacket.getDatagramPacketData()));
 		assertEquals(0, pulsePacket.getTypeIdentifier());
-		assertEquals(1, encrMsgPacket.getTypeIdentifier());
 		assertEquals(2, ackPacket.getTypeIdentifier());		
 	}
 	
@@ -101,22 +73,13 @@ public class TransportLayerTest {
 	
 	@Test
 	public void getMessageIDTest() {
-		assertEquals(20, TransportLayer.getMessageID(encrMsgPacket.getPayload().getPayloadData()));
-		assertEquals(20, TransportLayer.getMessageID(ackPacket.getPayload().getPayloadData()));
-		assertEquals(20, encryptedMessage.getMessageID());
+		assertEquals(20, TransportLayer.getMessageID(ackPacket.getPayload().getPayloadData(), Payload.ACKNOWLEDGEMENT));
 		assertEquals(20, acknowledgement.getMessageID());
 		
 	}
 	
 	@Test
 	public void getMessageLengthTest() {
-		assertEquals(14, TransportLayer.getMessageLength(encrMsgPacket.getPayload().getPayloadData(), 1));
-		assertEquals(14, encryptedMessage.getMessageLength());
-	}
-	
-	@Test
-	public void getEncryptedMessageTest() {
-		assertEquals("This is a test", TransportLayer.getPlainMessage(encrMsgPacket.getPayload().getPayloadData()));
-		assertEquals("This is a test", encryptedMessage.getEncryptedMessage());
+		assertEquals(5, TransportLayer.getMessageLength(globalMessage.getPayloadData(), Payload.GLOBAL_MESSAGE));
 	}
 }
