989e11690e8893afa3a1a9ba1a42449940cb9deb
justin <justin@justin-ThinkPad-P50>
2017-04-19 09:35:47 +0200

    Add javadoc to Payload.java

--- IntegrationProject/src/packet/Payload.java
+++ IntegrationProject/src/packet/Payload.java
@@ -2,12 +2,35 @@ package packet;
 
 public interface Payload {
 	
+	/**
+	 * The typeIdentifier of a <code>Pulse</code> payload.
+	 */
 	public static final int PULSE = 0;
+	
+	/**
+	 * The typeIdentifier of a <code>GlobalMessage</code> payload.
+	 */
 	public static final int GLOBAL_MESSAGE = 1;
+	
+	/**
+	 * The typeIdentifier of a <code>Acknowledgement</code> payload.
+	 */
 	public static final int ACKNOWLEDGEMENT = 2;
+	
+	/**
+	 * The typeIdentifier of a <code>EncryptionPair</code> payload.
+	 */
 	public static final int ENCRYPTION_PAIR = 3;
+	
+	/**
+	 * The typeIdentifier of a <code>EncryptedMessage</code> payload.
+	 */
 	public static final int ENCRYPTED_MESSAGE = 4;
 	
+	/**
+	 * Returns a byte array of the data of the <code>Payload</code>.
+	 * @return data the <code>Payload</code> data in a byte array
+	 */
 	public byte[] getPayloadData();
 
 }
